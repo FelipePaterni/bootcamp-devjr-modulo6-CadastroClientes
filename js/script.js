3f13cbfddcc10400e4b0e7d3c1d25279a6b2b0a5
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-07-21 13:39:51 -0300

    closes #3 - tratamento de erro

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
 function searchCEP() {
   var CEP = document.getElementById("CEPInput");
 
+  if (!CEP.value.trim()){
+    CEP.className = "form-control";
+    return
+  }
   var url = `https://viacep.com.br/ws/${CEP.value}/json/`;
 
-  try {
+  if (isValidCEP(CEP)) {
     $.getJSON(url, (data) => {
-
+        if(data.erro){
+         return showErro("Esse CEP não existe")
+        }
       showCEPInfo(data);
+      CEP.className = "form-control is-valid";
+    }).fail(() => {
+      showErro("Esse CEP não existe")
     });
-  } catch (error) {}
+  }else showErro("CEP inválido. Digite 8 números.")
 }
 
-function showCEPInfo(data){
-document.getElementById("EnderecoInput").value = data.logradouro;
-$("#NumeroInput").prop("disabled", false);
-document.getElementById("BairroInput").value = data.bairro;
-document.getElementById("CidadeInput").value = data.localidade;
-document.getElementById("EstadoInput").value = data.uf;
+function showCEPInfo(data) {
+  document.getElementById("EnderecoInput").value = data.logradouro;
+  $("#NumeroInput").prop("disabled", false);
+
+  document.getElementById("BairroInput").value = data.bairro;
+  document.getElementById("CidadeInput").value = data.localidade;
+  document.getElementById("EstadoInput").value = data.uf;
+}
+
+function showErro(textErro) {
+  document.getElementById("CEPInput").className = "form-control is-invalid";
+  document.getElementById("CEPFeedback").innerHTML = textErro;
+}
+function isValidCEP(cep) {
+  return /^[0-9]{8}$/.test(cep);
 }
