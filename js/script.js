5ba84eed6db24ec219c5104d5fead8183fd40f84
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-07-21 15:08:30 -0300

    closes #6 - dados salvos na tabela

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $("#CEPInput").mask("00000-000");
+var listClientes = [];
+var validCEP = false;
 
 function searchCEP() {
   var CEP = document.getElementById("CEPInput");
@@ -8,7 +10,7 @@ function searchCEP() {
   cleanForm();
   if (!valueCEP.trim()) {
     CEP.className = "form-control";
-    
+    valueCEP = false;
     return;
   }
 
@@ -19,6 +21,7 @@ function searchCEP() {
       }
       showCEPInfo(data);
       CEP.className = "form-control is-valid";
+      validCEP = true;
     }).fail(() => {
       showErro("Esse CEP não existe");
     });
@@ -26,7 +29,7 @@ function searchCEP() {
 }
 
 function showCEPInfo(data) {
-  document.getElementById("EnderecoInput").value = data.logradouro ;
+  document.getElementById("EnderecoInput").value = data.logradouro;
   $("#NumeroInput").prop("disabled", false);
 
   document.getElementById("BairroInput").value = data.bairro;
@@ -46,7 +49,71 @@ function cleanForm() {
 function showErro(textErro) {
   document.getElementById("CEPInput").className = "form-control is-invalid";
   document.getElementById("CEPFeedback").innerHTML = textErro;
+  validCEP = false;
 }
 function isValidCEP(cep) {
   return /^[0-9]{8}$/.test(cep);
+}
+
+function save() {
+    if(!validCEP) {
+        alert("CEP invalido");
+        return
+    }
+  var newCliente = {
+    id: listClientes.length + 1,
+    nome: `${document.getElementById("NomeInput").value} ${
+      document.getElementById("SobrenomeInput").value
+    }`,
+    endereco: document.getElementById("EnderecoInput").value + (document.getElementById("NumeroInput").value.trim() ? `, ${document.getElementById("NumeroInput").value.trim()}` :""),
+    cep: document.getElementById("CEPInput").value,
+    bairro: document.getElementById("BairroInput").value,
+    cidade: document.getElementById("CidadeInput").value,
+    estado: document.getElementById("EstadoInput").value,
+  };
+  addNewRow(newCliente);
+  listClientes.push(newCliente);
+  document.getElementById("formClientes").reset();
+}
+
+//on load
+loadAlunos();
+
+function loadAlunos() {
+  for (let cliente of listClientes) {
+    addNewRow(cliente);
+  }
+}
+
+function addNewRow(cliente) {
+  var table = document.getElementById("clientesTable");
+  var row = table.insertRow();
+
+  //id
+  var idNode = document.createTextNode(cliente.id);
+  row.insertCell().appendChild(idNode);
+
+  //nome
+  var nomeNode = document.createTextNode(cliente.nome);
+  row.insertCell().appendChild(nomeNode);
+
+  //endereço
+  var enderecoNode = document.createTextNode(cliente.endereco);
+  row.insertCell().appendChild(enderecoNode);
+
+  //cep
+  var cepNode = document.createTextNode(cliente.cep);
+  row.insertCell().appendChild(cepNode);
+
+  //bairro
+  var bairroNode = document.createTextNode(cliente.bairro);
+  row.insertCell().appendChild(bairroNode);
+
+  //cidade
+  var cidadeNode = document.createTextNode(cliente.cidade);
+  row.insertCell().appendChild(cidadeNode);
+
+  //estado
+  var estadoNode = document.createTextNode(cliente.estado);
+  row.insertCell().appendChild(estadoNode);
 }
